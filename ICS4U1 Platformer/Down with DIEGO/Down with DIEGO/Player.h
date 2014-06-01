21e36eda587c5daa4371d9e25b22437cf1607899
anthgithub <anthnintendofan@gmail.com>
2014-06-01 14:50:56 -0400

    Some Collision Fixes
    I "fixed" some collisions if you would call it that. Jump is also less
    floaty and the player moves quicker

--- ICS4U1 Platformer/Down with DIEGO/Down with DIEGO/Player.h
+++ ICS4U1 Platformer/Down with DIEGO/Down with DIEGO/Player.h
@@ -23,7 +23,7 @@ public:
 	{
 		if(vely == 0)
 		{
-			vely -= 5;
+			vely -= 7;
 		}
 	}
 	void update(float dt)
@@ -32,47 +32,87 @@ public:
 		float tempy = y + vely * dt;//The player's temporary y
 		int xdir = velx > 0 ? 1 : 0; // get x-sign using ternary operator
 		int ydir = vely < 0 ? 1 : 0; // get y-sign using ternary operator
-		velx *= 0.995;
-		vely += 4.9*dt;
-
+		velx *= 0.965;
+		vely += 20*dt;
+	
 		bool q1 = world->tiles[int(tempx + xdir) + int(tempy + ydir) * 20] != 0;//Where the first quadrant is
 		bool q2 = world->tiles[int(tempx + xdir) + int(tempy + 1 - ydir) * 20] != 0;//Where the second quadrant is
 		bool q3 = world->tiles[int(tempx + 1 - xdir) + int(tempy + 1 - ydir) * 20] != 0;//Where the third quadrant is
-
 		float ttempx = tempx, ttempy = tempy;
-
-		if(velx)
+		float xpen;
+		float ypen;
+		if(q1) //Checks for collision in the first quadrant
 		{
-			if(q1 || (q2 && !q3)) //Checks for collision in the first quadrant
+			ttempx = xdir ? floor(tempx) : ceil(tempx);
+			ttempy = ydir ? ceil(tempy) : floor(tempy);
+			xpen = abs(ttempx - tempx); 
+			ypen = abs(ttempy - tempy);	
+			if(ypen > xpen)
 			{
-				ttempx = xdir ? floor(tempx) : ceil(tempx);
+				tempx = ttempx;
 			}
-		}
+			else if(ypen < xpen)
+			{
+				tempy = ttempy;
+			}
 
-		if(vely)
+		}
+		if(q2) //Checks for collision in the second quadrant
 		{
-			if(q3 || (q2 && !q1))//Checks for collision in the third quadrant
+			ttempx = xdir ? floor(tempx) : ceil(tempx);
+			ttempy = ydir ? ceil(tempy) : floor(tempy);
+			xpen = abs(ttempx - tempx); 
+			ypen = abs(ttempy - tempy);	
+			if(ypen > xpen)
 			{
-				vely = 0;
-				ttempy = ydir ? ceil(tempy) : floor(tempy);
+				tempx = ttempx;
 			}
-		}
+			else if(ypen < xpen)
+			{
+				tempy = ttempy;
+			}
 
-		/*
-		float xpen = abs(ttempx - tempx); 
-		float ypen = abs(ttempy - tempy);
+		}
+		if(q3) //Checks for collision in the third quadrant
+		{
+			ttempx = xdir ? floor(tempx) : ceil(tempx);
+			ttempy = ydir ? ceil(tempy) : floor(tempy);
+			xpen = abs(ttempx - tempx); 
+			ypen = abs(ttempy - tempy);	
+			if(ypen > xpen)
+			{
+				tempx = ttempx;
+			}
+			else if(ypen < xpen)
+			{
+				tempy = ttempy;
+			}
+
+		}
+	
+
+
+		if(q3 || (q2 && !q1))//Checks for collision in the third quadrant
+		{
+			vely = 0;
+			ttempy = ydir ? ceil(tempy) : floor(tempy);
+		}
+			
+		xpen = abs(ttempx - tempx); 
+		ypen = abs(ttempy - tempy);
 
 		if(ypen > xpen)
 		{
-			tempy = ttempy;
+			tempx += xpen;
 		}
 		else if(ypen < xpen)
 		{
-			tempx = ttempx;
-		}*/
+			tempy += ypen;
+		}
 
 		x = tempx;
 		y = tempy;
+	
 	}
 };
 #endif
